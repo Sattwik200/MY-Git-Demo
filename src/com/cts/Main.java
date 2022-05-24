27829f9424507d4c28627c8fcff8cca0eadf6c91
Sattwik200 <Swats@git2000>
2022-05-24 14:54:21 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,12 @@
+package com.cts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("How are you??");
+
+	}
+
+}
