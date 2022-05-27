da67ccc4bc47494fbdee548f56a6029e9fd40ef1
username <Swats@git2000>
2022-05-27 12:16:36 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,11 +1,16 @@
 package com.cts;
 
+import com.service.ServiceClass;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("How are you??");
+		
+		ServiceClass obj = new ServiceClass();
+		System.out.println(obj.findLength("password"));
 
 	}
 
---
+++ src/com/service/ServiceClass.java
@@ -0,0 +1,8 @@
+package com.service;
+
+public class ServiceClass {
+	public int findLength(String word)
+	{
+		return word.length();
+	}
+}
